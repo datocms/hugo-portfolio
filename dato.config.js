afd2e690c8dde3f07b78f5fb73d74b371600a011
Stefano Verna <s.verna@cantierecreativo.net>
2019-10-08 12:05:29 +0200

    First commit

---
+++ dato.config.js
@@ -0,0 +1,95 @@
+const htmlTag = require('html-tag');
+
+// This function helps transforming structures like:
+//
+// [{ tagName: 'meta', attributes: { name: 'description', content: 'foobar' } }]
+//
+// into proper HTML tags:
+//
+// <meta name="description" content="foobar" />
+
+const toHtml = (tags) => (
+  tags.map(({ tagName, attributes, content }) => (
+    htmlTag(tagName, attributes, content)
+  )).join("")
+);
+
+// Arguments that will receive the mapping function:
+//
+// * dato: lets you easily access any content stored in your DatoCMS
+//   administrative area;
+//
+// * root: represents the root of your project, and exposes commands to
+//   easily create local files/directories;
+//
+// * i18n: allows to switch the current locale to get back content in
+//   alternative locales from the first argument.
+//
+// Read all the details here:
+// https://github.com/datocms/js-datocms-client/blob/master/docs/dato-cli.md
+
+module.exports = (dato, root, i18n) => {
+
+  // Add to the existing Hugo config files some properties coming from data
+  // stored on DatoCMS
+  ['config.dev.toml', 'config.prod.toml'].forEach(file => {
+    root.addToDataFile(file, 'toml', {
+      title: dato.site.globalSeo.siteName,
+      languageCode: i18n.locale
+    });
+  });
+
+  // Create a YAML data file to store global data about the site
+  root.createDataFile('data/settings.yml', 'yaml', {
+    name: dato.site.globalSeo.siteName,
+    language: dato.site.locales[0],
+    intro: dato.home.introText,
+    copyright: dato.home.copyright,
+    // iterate over all the `social_profile` item types
+    socialProfiles: dato.socialProfiles.map(profile => {
+      return {
+        type: profile.profileType.toLowerCase().replace(/ +/, '-'),
+        url: profile.url,
+      };
+    }),
+    faviconMetaTags: toHtml(dato.site.faviconMetaTags),
+    seoMetaTags: toHtml(dato.home.seoMetaTags)
+  });
+
+  // Create a markdown file with content coming from the `about_page` item
+  // type stored in DatoCMS
+  root.createPost(`content/about.md`, 'yaml', {
+    frontmatter: {
+      title: dato.aboutPage.title,
+      subtitle: dato.aboutPage.subtitle,
+      photo: dato.aboutPage.photo.url({ w: 800, fm: 'jpg', auto: 'compress' }),
+      seoMetaTags: toHtml(dato.aboutPage.seoMetaTags),
+      menu: { main: { weight: 100 } }
+    },
+    content: dato.aboutPage.bio
+  });
+
+  // Create a `work` directory (or empty it if already exists)...
+  root.directory('content/works', dir => {
+    // ...and for each of the works stored online...
+    dato.works.forEach((work, index) => {
+      // ...create a markdown file with all the metadata in the frontmatter
+      dir.createPost(`${work.slug}.md`, 'yaml', {
+        frontmatter: {
+          title: work.title,
+          coverImage: work.coverImage.url({ w: 450, fm: 'jpg', auto: 'compress' }),
+          image: work.coverImage.url({ fm: 'jpg', auto: 'compress' }),
+          detailImage: work.coverImage.url({ w: 600, fm: 'jpg', auto: 'compress' }),
+          excerpt: work.excerpt,
+          seoMetaTags: toHtml(work.seoMetaTags),
+          extraImages: work.gallery.map(item =>
+            item.url({ h: 300, fm: 'jpg', auto: 'compress' })
+          ),
+          weight: index
+        },
+        content: work.description
+      });
+    });
+  });
+};
+
---
+++ webpack.prod.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    all: __dirname + '/assets/js/index.js',
+  },
+  resolve: {
+    modules: [path.resolve(__dirname + '/assets/js'), 'node_modules'],
+  },
+  output: {
+    path: __dirname + '/public/assets',
+    filename: '[name].js',
+    publicPath: '/assets',
+  },
+  module: {
+    rules: [
+      {
+        test: /.*\.sass$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+          { loader: 'import-glob-loader' }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'all.css' }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }
+    })
+  ]
+};
+
